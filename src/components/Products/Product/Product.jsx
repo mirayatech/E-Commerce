11bcc8ac2db4c78e4ea04e6e66d3c27f967c9eec
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-24 10:39:44 +0200

    creating and styling product layout

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+} from "@mui/material";
+
+import { AddShoppingCart } from "@mui/icons-material";
+// import useStyles from "./styles";
+
+const Product = ({ product }) => {
+  // const classes = useStyles();
+  return (
+    <Card sx={{ maxWidth: "100%" }}>
+      <CardMedia
+        sx={{ height: 0, paddingTop: "56.25%" }}
+        image=""
+        title={product.name}
+      />
+
+      <CardContent>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography variant="h5">{product.name}</Typography>
+          <Typography variant="h5">{product.price} </Typography>
+        </CardContent>
+        <Typography variant="h2" color="textSecondary">
+          {" "}
+          {product.description}
+        </Typography>
+      </CardContent>
+
+      <CardActions
+        disableSpacing
+        sx={{ display: "flex", justifyContent: "flex-end" }}
+      >
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
