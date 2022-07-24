1ffbcfbe58e3d9260803f2f727de99fc4bf607ea
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-24 11:27:49 +0200

    link in image

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,15 +5,20 @@ import { Grid } from "@mui/material";
 const products = [
   {
     id: 1,
-    name: "Shoes",
-    description: "Running shoes",
+    name: "Air Jordan 1",
+    description: "Sneaker shoes",
     price: "$5",
+    image:
+      "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2021%2F12%2FAir-Jordan-1-High-Zoom-CMFT-Pink-Oxford-0.jpg?w=960&cbr=1&q=90&fit=max",
   },
   {
     id: 2,
     name: "Macbook",
     description: "Apple Macbook",
     price: "$100",
+
+    image:
+      "https://www.apple.com/newsroom/images/tile-images/Apple_16-inch-MacBook-Pro_111319.jpg.news_app_ed.jpg",
   },
 ];
 
