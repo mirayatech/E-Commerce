683367ce7797976858d323ac7857f993752e86d6
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-25 08:13:01 +0200

    fetching cart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import { commerce } from "./library/commerce";
 import { Navbar, Products } from "./components";
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  // Get the products
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
+  console.log(cart);
+
   return (
     <div>
       <Navbar />
