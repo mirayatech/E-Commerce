e4af97aaa87947ca818e351ccd55fc04ab2da662
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-29 09:23:12 +0200

    creating the select field layout

--- src/components/AdressForm.jsx
+++ src/components/AdressForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   inputLabel,
   Select,
@@ -6,6 +6,7 @@ import {
   Button,
   Grid,
   Typography,
+  InputLabel,
 } from "@mui/material";
 
 import { useForm, FormProvider } from "react-hook-form";
@@ -28,6 +29,32 @@ function AdressForm() {
             <FormInput required name="email" label="Email" />
             <FormInput required name="city" label="City" />
             <FormInput required name="zip" label="ZIP / Postal Code" />
+
+            {/* SelectField */}
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={""} fullWidth onChange={""}>
+                <MenuItem key={""} value={""}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select value={""} fullWidth onChange={""}>
+                <MenuItem key={""} value={""}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={""} fullWidth onChange={""}>
+                <MenuItem key={""} value={""}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
           </Grid>
         </form>
       </FormProvider>
