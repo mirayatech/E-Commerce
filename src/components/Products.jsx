85759ac23e0233678cf6cfa7b8d7ef5fb6cef738
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-30 14:46:36 +0200

    refactor(Products): cleaning code

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import Product from './Product'
-import { Grid } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 
 const Products = ({ products, onAddToCart }) => {
   return (
-    <main
-      style={{
-        flexGrow: '1',
-        paddingTop: '80px',
+    <Container
+      sx={{
         backgroundColor: '#fafafa',
       }}
     >
-      <div />
-      <Grid container spacing={2} justifyContent="center">
+      <Typography variant="h3" component="h1" textAlign="center" mt={3}>
+        Products
+      </Typography>
+      <Grid container spacing={2} justifyContent="center" mt={2}>
         {products.map((product) => (
-          <Grid item key={product.id} xs={10} sm={6} md={4} lg={2.5}>
+          <Grid item key={product.id} xs={10} sm={6} md={4} lg={3}>
             <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
-    </main>
+    </Container>
   )
 }
 
