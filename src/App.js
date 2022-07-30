746d730a3bbd1829d1fc0826236536b2bcd87dca
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-30 14:44:21 +0200

    refactor(APP): cleaning code and deleting comments

--- src/App.js
+++ src/App.js
@@ -7,73 +7,56 @@ const App = () => {
   const [products, setProducts] = useState([])
   const [cart, setCart] = useState({})
   const [order, setOrder] = useState({})
-  const [errorMessage, setErrorMessage] = useState('')
 
-  // fetching products
   const fetchProducts = async () => {
     const { data } = await commerce.products.list()
     setProducts(data)
   }
 
-  // fetching cart
   const fetchCart = async () => {
     setCart(await commerce.cart.retrieve())
   }
 
-  // Add Product to cart
   const handleAddToCart = async (productId, quantity) => {
     const item = await commerce.cart.add(productId, quantity)
     setCart(item.cart)
   }
 
-  // Updating product quantity
   const handleUpdateCartQty = async (lineItemId, quantity) => {
     const response = await commerce.cart.update(lineItemId, { quantity })
     setCart(response.cart)
   }
 
-  // Remove product from cart
   const handleRemoveFromCart = async (lineItemId) => {
     const response = await commerce.cart.remove(lineItemId)
     setCart(response.cart)
   }
 
-  // Empty cart
   const handleEmptyCart = async () => {
     const response = await commerce.cart.empty()
     setCart(response.cart)
   }
 
-  // Refresh Cart
   const refreshCart = async () => {
     const newCart = await commerce.cart.refresh()
     setCart(newCart)
   }
 
-  // Get all data when checking out
   const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
-    try {
-      const incomingOrder = await commerce.checkout.capture(
-        checkoutTokenId,
-        newOrder
-      )
-      refreshCart()
-
-      setOrder(incomingOrder)
-    } catch (error) {
-      refreshCart()
-
-      setErrorMessage(error.data.error.message)
-    }
+    const incomingOrder = await commerce.checkout.capture(
+      checkoutTokenId,
+      newOrder
+    )
+    refreshCart()
+    setOrder(incomingOrder)
   }
 
   useEffect(() => {
-    fetchProducts()
-    fetchCart()
+    Promise.all([fetchCart(), fetchProducts()])
   }, [])
 
   return (
-    <div>
+    <>
       <Navbar totalItems={cart.total_items} />
       <Routes>
         <Route
@@ -81,7 +64,7 @@ const App = () => {
           element={
             <Products products={products} onAddToCart={handleAddToCart} />
           }
-        ></Route>
+        />
 
         <Route
           path="/cart"
@@ -93,7 +76,8 @@ const App = () => {
               handleEmptyCart={handleEmptyCart}
             />
           }
-        ></Route>
+        />
+
         <Route
           path="/Checkout"
           element={
@@ -101,12 +85,11 @@ const App = () => {
               cart={cart}
               order={order}
               onCaptureCheckout={handleCaptureCheckout}
-              error={errorMessage}
             />
           }
-        ></Route>
+        />
       </Routes>
-    </div>
+    </>
   )
 }
 
