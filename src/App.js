91a11d38294a45b67c7f164b2c338cdb5f53d502
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-30 11:23:38 +0200

    format document

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { commerce } from "./library/commerce";
-import { Products, Navbar, Cart, Checkout } from "./components";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { commerce } from './library/commerce'
+import { Products, Navbar, Cart, Checkout } from './components'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({});
-  const [order, setOrder] = useState({});
-  const [errorMessage, setErrorMessage] = useState("");
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
+  const [order, setOrder] = useState({})
+  const [errorMessage, setErrorMessage] = useState('')
 
   // fetching products
   const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
-    setProducts(data);
-  };
+    const { data } = await commerce.products.list()
+    setProducts(data)
+  }
 
   // fetching cart
   const fetchCart = async () => {
-    setCart(await commerce.cart.retrieve());
-  };
+    setCart(await commerce.cart.retrieve())
+  }
 
   // Add Product to cart
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
-  };
+    const item = await commerce.cart.add(productId, quantity)
+    setCart(item.cart)
+  }
 
   // Updating product quantity
   const handleUpdateCartQty = async (lineItemId, quantity) => {
-    const response = await commerce.cart.update(lineItemId, { quantity });
-    setCart(response.cart);
-  };
+    const response = await commerce.cart.update(lineItemId, { quantity })
+    setCart(response.cart)
+  }
 
   // Remove product from cart
   const handleRemoveFromCart = async (lineItemId) => {
-    const response = await commerce.cart.remove(lineItemId);
-    setCart(response.cart);
-  };
+    const response = await commerce.cart.remove(lineItemId)
+    setCart(response.cart)
+  }
 
   // Empty cart
   const handleEmptyCart = async () => {
-    const response = await commerce.cart.empty();
-    setCart(response.cart);
-  };
+    const response = await commerce.cart.empty()
+    setCart(response.cart)
+  }
 
   // Refresh Cart
   const refreshCart = async () => {
-    const newCart = await commerce.cart.refresh();
-    setCart(newCart);
-  };
+    const newCart = await commerce.cart.refresh()
+    setCart(newCart)
+  }
 
   // Get all data when checking out
   const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
@@ -56,21 +56,21 @@ const App = () => {
       const incomingOrder = await commerce.checkout.capture(
         checkoutTokenId,
         newOrder
-      );
-      refreshCart();
+      )
+      refreshCart()
 
-      setOrder(incomingOrder);
+      setOrder(incomingOrder)
     } catch (error) {
-      refreshCart();
+      refreshCart()
 
-      setErrorMessage(error.data.error.message);
+      setErrorMessage(error.data.error.message)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchProducts();
-    fetchCart();
-  }, []);
+    fetchProducts()
+    fetchCart()
+  }, [])
 
   return (
     <div>
@@ -107,7 +107,7 @@ const App = () => {
         ></Route>
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
