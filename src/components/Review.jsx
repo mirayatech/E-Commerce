4320c61ec49a553ed3858deac6a37c7aa1e8a2a8
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-30 17:39:17 +0200

    refactoring Review file

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,31 +1,35 @@
-import React from "react";
-import { Typography, List, ListItem, ListItemText } from "@mui/material";
+import React from 'react'
+import { Typography, List, ListItem, ListItemText } from '@mui/material'
 
 const Review = ({ checkoutToken }) => (
   <>
-    <Typography variant="h6" gutterBottom>
+    <Typography variant="h6" component="h2" gutterBottom>
       Order summary
     </Typography>
+
     <List disablePadding>
-      {checkoutToken.live.line_items.map((product) => (
-        <ListItem style={{ padding: "10px 0" }} key={product.name}>
-          <ListItemText
-            primary={product.name}
-            secondary={`Quantity: ${product.quantity}`}
-          />
-          <Typography variant="body2">
-            {product.line_total.formatted_with_symbol}
-          </Typography>
-        </ListItem>
-      ))}
-      <ListItem style={{ padding: "10px 0" }}>
+      {checkoutToken.live.line_items.map(
+        ({ id, name, quantity, line_total: { formatted_with_symbol } }) => (
+          <ListItem sx={{ py: 3 }} key={id}>
+            <ListItemText primary={name} secondary={`Quantity: ${quantity}`} />
+            <Typography variant="body2" component="h2">
+              {formatted_with_symbol}
+            </Typography>
+          </ListItem>
+        )
+      )}
+      <ListItem>
         <ListItemText primary="Total" />
-        <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
+        <Typography
+          variant="subtitle1"
+          component="h3"
+          sx={{ paddingBottom: '20px', fontWeight: 700 }}
+        >
           {checkoutToken.live.subtotal.formatted_with_symbol}
         </Typography>
       </ListItem>
     </List>
   </>
-);
+)
 
-export default Review;
+export default Review
