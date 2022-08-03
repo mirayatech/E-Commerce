6e67b4ee57a8aca272dba2078e1e9be9274df523
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-03 20:49:01 +0200

    convert to typescript

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+const root = createRoot(document.getElementById('root') as Element)
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
